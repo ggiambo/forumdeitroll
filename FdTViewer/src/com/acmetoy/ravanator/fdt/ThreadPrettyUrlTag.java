68f670453e1f73816cd798b5ea265a888ad549f5
achille <achille@fdt>
2012-02-06 02:53:21 +0100

    tolti commenti del decompilatore :$

--- FdTViewer/src/com/acmetoy/ravanator/fdt/ThreadPrettyUrlTag.java
+++ FdTViewer/src/com/acmetoy/ravanator/fdt/ThreadPrettyUrlTag.java
@@ -1,77 +1,73 @@
-/*    */ package com.acmetoy.ravanator.fdt;
-/*    */ 
-/*    */ import javax.servlet.ServletContext;
-/*    */ import javax.servlet.jsp.JspException;
-/*    */ import javax.servlet.jsp.JspWriter;
-/*    */ import javax.servlet.jsp.PageContext;
-/*    */ import javax.servlet.jsp.tagext.TagSupport;
-/*    */ import org.apache.log4j.Logger;
-/*    */ 
-/*    */ public class ThreadPrettyUrlTag extends TagSupport
-/*    */ {
-/* 13 */   private static Logger LOG = Logger.getLogger(ThreadPrettyUrlTag.class);
-/*    */   private String threadId;
-/*    */   private String subject;
-/*    */   private String msgId;
-/*    */ 
-/*    */   private static String prettifySubject(String subject)
-/*    */   {
-/* 16 */     StringBuilder out = new StringBuilder();
-/* 17 */     for (char c : subject.toCharArray()) {
-/* 18 */       if ((Character.isAlphabetic(c)) || (Character.isDigit(c)))
-/* 19 */         out.append(c);
-/*    */       else {
-/* 21 */         out.append('-');
-/*    */       }
-/*    */     }
-/* 24 */     return out.toString();
-/*    */   }
-/*    */ 
-/*    */   public int doEndTag() throws JspException
-/*    */   {
-/*    */     try {
-/* 30 */       String prettySubject = prettifySubject(this.subject);
-/* 31 */       JspWriter out = this.pageContext.getOut();
-/* 32 */       out.write("<a href=\"");
-/* 33 */       out.write(this.pageContext.getServletContext().getContextPath());
-/* 34 */       out.write("/thread/");
-/* 35 */       out.write(this.threadId);
-/* 36 */       out.write("/");
-/* 37 */       out.write(prettySubject);
-/* 38 */       out.write("#msg");
-/* 39 */       out.write(this.msgId);
-/* 40 */       out.write("\">");
-/* 41 */       out.write(this.subject);
-/* 42 */       out.write("</a>");
-/* 43 */       out.flush();
-/*    */     } catch (Exception e) {
-/* 45 */       LOG.error(e.getMessage(), e);
-/*    */     }
-/* 47 */     return 0;
-/*    */   }
-/*    */ 
-/*    */   public String getThreadId()
-/*    */   {
-/* 54 */     return this.threadId;
-/*    */   }
-/*    */   public void setThreadId(String threadId) {
-/* 57 */     this.threadId = threadId;
-/*    */   }
-/*    */   public String getSubject() {
-/* 60 */     return this.subject;
-/*    */   }
-/*    */   public void setSubject(String subject) {
-/* 63 */     this.subject = subject;
-/*    */   }
-/*    */   public String getMsgId() {
-/* 66 */     return this.msgId;
-/*    */   }
-/*    */   public void setMsgId(String msgId) {
-/* 69 */     this.msgId = msgId;
-/*    */   }
-/*    */ }
+package com.acmetoy.ravanator.fdt;
 
-/* Location:           /home/daniele/war/WEB-INF/classes/
- * Qualified Name:     com.acmetoy.ravanator.fdt.ThreadPrettyUrlTag
- * JD-Core Version:    0.6.0
- */
+import javax.servlet.ServletContext;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.TagSupport;
+import org.apache.log4j.Logger;
+
+public class ThreadPrettyUrlTag extends TagSupport
+{
+  private static Logger LOG = Logger.getLogger(ThreadPrettyUrlTag.class);
+  private String threadId;
+  private String subject;
+  private String msgId;
+
+  private static String prettifySubject(String subject)
+  {
+    StringBuilder out = new StringBuilder();
+    for (char c : subject.toCharArray()) {
+      if ((Character.isAlphabetic(c)) || (Character.isDigit(c)))
+        out.append(c);
+      else {
+        out.append('-');
+      }
+    }
+    return out.toString();
+  }
+
+  public int doEndTag() throws JspException
+  {
+    try {
+      String prettySubject = prettifySubject(this.subject);
+      JspWriter out = this.pageContext.getOut();
+      out.write("<a href=\"");
+      out.write(this.pageContext.getServletContext().getContextPath());
+      out.write("/thread/");
+      out.write(this.threadId);
+      out.write("/");
+      out.write(prettySubject);
+      out.write("#msg");
+      out.write(this.msgId);
+      out.write("\">");
+      out.write(this.subject);
+      out.write("</a>");
+      out.flush();
+    } catch (Exception e) {
+      LOG.error(e.getMessage(), e);
+    }
+    return 0;
+  }
+
+  public String getThreadId()
+  {
+    return this.threadId;
+  }
+  public void setThreadId(String threadId) {
+    this.threadId = threadId;
+  }
+  public String getSubject() {
+    return this.subject;
+  }
+  public void setSubject(String subject) {
+    this.subject = subject;
+  }
+  public String getMsgId() {
+    return this.msgId;
+  }
+  public void setMsgId(String msgId) {
+    this.msgId = msgId;
+  }
+}
+
--- FdTViewer/src/com/acmetoy/ravanator/fdt/filters/PrettyUrlFilter.java
+++ FdTViewer/src/com/acmetoy/ravanator/fdt/filters/PrettyUrlFilter.java
@@ -1,90 +1,86 @@
-/*    */ package com.acmetoy.ravanator.fdt.filters;
-/*    */ 
-/*    */ import java.io.IOException;
-/*    */ import java.util.regex.Matcher;
-/*    */ import java.util.regex.Pattern;
-/*    */ import javax.servlet.Filter;
-/*    */ import javax.servlet.FilterChain;
-/*    */ import javax.servlet.FilterConfig;
-/*    */ import javax.servlet.RequestDispatcher;
-/*    */ import javax.servlet.ServletContext;
-/*    */ import javax.servlet.ServletException;
-/*    */ import javax.servlet.ServletRequest;
-/*    */ import javax.servlet.ServletResponse;
-/*    */ import javax.servlet.http.HttpServletRequest;
-/*    */ import javax.servlet.http.HttpServletResponse;
-/*    */ import org.apache.log4j.Logger;
-/*    */ 
-/*    */ public class PrettyUrlFilter
-/*    */   implements Filter
-/*    */ {
-/* 19 */   private Pattern threadPattern = Pattern.compile("/thread/([0-9]{6,8})/.*");
-/*    */ 
-/* 21 */   private String images = "/images/";
-/* 22 */   private String ctx_images = null;
-/* 23 */   private String js = "/js/";
-/* 24 */   private String ctx_js = null;
-/* 25 */   private String css = "/css/";
-/* 26 */   private String ctx_css = null;
-/*    */ 
-/* 28 */   private static Logger LOG = Logger.getLogger(PrettyUrlFilter.class);
-/*    */ 
-/*    */   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
-/*    */   {
-/* 32 */     HttpServletRequest hsRequest = (HttpServletRequest)request;
-/* 33 */     HttpServletResponse hsResponse = (HttpServletResponse)response;
-/* 34 */     String requestUri = hsRequest.getRequestURI();
-/* 35 */     if ((requestUri.contains(this.images)) && (!requestUri.startsWith(this.ctx_images)))
-/*    */     {
-/* 37 */       String real_url = hsRequest.getContextPath() + requestUri.substring(requestUri.indexOf(this.images));
-/* 38 */       if (request.getParameter("v") != null) {
-/* 39 */         real_url = real_url + "?v=" + request.getParameter("v");
-/*    */       }
-/* 41 */       LOG.debug(requestUri + " -> " + real_url);
-/* 42 */       hsResponse.sendRedirect(real_url);
-/* 43 */       return;
-/* 44 */     }if ((requestUri.contains(this.js)) && (!requestUri.startsWith(this.ctx_js)))
-/*    */     {
-/* 46 */       String real_url = hsRequest.getContextPath() + requestUri.substring(requestUri.indexOf(this.js));
-/* 47 */       if (request.getParameter("v") != null) {
-/* 48 */         real_url = real_url + "?v=" + request.getParameter("v");
-/*    */       }
-/* 50 */       LOG.debug(requestUri + " -> " + real_url);
-/* 51 */       hsResponse.sendRedirect(real_url);
-/* 52 */       return;
-/* 53 */     }if ((requestUri.contains(this.css)) && (!requestUri.startsWith(this.ctx_css))) {
-/* 54 */       String real_url = hsRequest.getContextPath() + requestUri.substring(requestUri.indexOf(this.css));
-/* 55 */       if (request.getParameter("v") != null) {
-/* 56 */         real_url = real_url + "?v=" + request.getParameter("v");
-/*    */       }
-/* 58 */       LOG.debug(requestUri + " -> " + real_url);
-/* 59 */       hsResponse.sendRedirect(real_url);
-/* 60 */       return;
-/* 61 */     }if (requestUri.startsWith(hsRequest.getContextPath() + "/thread/"))
-/*    */     {
-/* 63 */       Matcher matcher = this.threadPattern.matcher(requestUri);
-/* 64 */       if (matcher.find()) {
-/* 65 */         String threadId = matcher.group(1);
-/* 66 */         String real_url = "/Threads?action=getByThread&threadId=" + threadId;
-/* 67 */         LOG.debug(requestUri + " -> " + real_url);
-/* 68 */         request.getRequestDispatcher(real_url).forward(request, response);
-/* 69 */         return;
-/*    */       }
-/*    */     }
-/* 72 */     chain.doFilter(request, response);
-/*    */   }
-/*    */ 
-/*    */   public void destroy() {
-/*    */   }
-/*    */ 
-/*    */   public void init(FilterConfig config) throws ServletException {
-/* 79 */     this.ctx_images = (config.getServletContext().getContextPath() + this.images);
-/* 80 */     this.ctx_js = (config.getServletContext().getContextPath() + this.js);
-/* 81 */     this.ctx_css = (config.getServletContext().getContextPath() + this.css);
-/*    */   }
-/*    */ }
+package com.acmetoy.ravanator.fdt.filters;
 
-/* Location:           /home/daniele/war/WEB-INF/classes/
- * Qualified Name:     com.acmetoy.ravanator.fdt.filters.PrettyUrlFilter
- * JD-Core Version:    0.6.0
- */
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.Logger;
+
+public class PrettyUrlFilter
+  implements Filter
+{
+  private Pattern threadPattern = Pattern.compile("/thread/([0-9]{6,8})/.*");
+
+  private String images = "/images/";
+  private String ctx_images = null;
+  private String js = "/js/";
+  private String ctx_js = null;
+  private String css = "/css/";
+  private String ctx_css = null;
+
+  private static Logger LOG = Logger.getLogger(PrettyUrlFilter.class);
+
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
+  {
+    HttpServletRequest hsRequest = (HttpServletRequest)request;
+    HttpServletResponse hsResponse = (HttpServletResponse)response;
+    String requestUri = hsRequest.getRequestURI();
+    if ((requestUri.contains(this.images)) && (!requestUri.startsWith(this.ctx_images)))
+    {
+      String real_url = hsRequest.getContextPath() + requestUri.substring(requestUri.indexOf(this.images));
+      if (request.getParameter("v") != null) {
+        real_url = real_url + "?v=" + request.getParameter("v");
+      }
+      LOG.debug(requestUri + " -> " + real_url);
+      hsResponse.sendRedirect(real_url);
+      return;
+    }if ((requestUri.contains(this.js)) && (!requestUri.startsWith(this.ctx_js)))
+    {
+      String real_url = hsRequest.getContextPath() + requestUri.substring(requestUri.indexOf(this.js));
+      if (request.getParameter("v") != null) {
+        real_url = real_url + "?v=" + request.getParameter("v");
+      }
+      LOG.debug(requestUri + " -> " + real_url);
+      hsResponse.sendRedirect(real_url);
+      return;
+    }if ((requestUri.contains(this.css)) && (!requestUri.startsWith(this.ctx_css))) {
+      String real_url = hsRequest.getContextPath() + requestUri.substring(requestUri.indexOf(this.css));
+      if (request.getParameter("v") != null) {
+        real_url = real_url + "?v=" + request.getParameter("v");
+      }
+      LOG.debug(requestUri + " -> " + real_url);
+      hsResponse.sendRedirect(real_url);
+      return;
+    }if (requestUri.startsWith(hsRequest.getContextPath() + "/thread/"))
+    {
+      Matcher matcher = this.threadPattern.matcher(requestUri);
+      if (matcher.find()) {
+        String threadId = matcher.group(1);
+        String real_url = "/Threads?action=getByThread&threadId=" + threadId;
+        LOG.debug(requestUri + " -> " + real_url);
+        request.getRequestDispatcher(real_url).forward(request, response);
+        return;
+      }
+    }
+    chain.doFilter(request, response);
+  }
+
+  public void destroy() {
+  }
+
+  public void init(FilterConfig config) throws ServletException {
+    this.ctx_images = (config.getServletContext().getContextPath() + this.images);
+    this.ctx_js = (config.getServletContext().getContextPath() + this.js);
+    this.ctx_css = (config.getServletContext().getContextPath() + this.css);
+  }
+}
+
