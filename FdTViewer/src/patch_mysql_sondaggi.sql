919ce68defdab2ac707767aeea7c27609e8e25fe
giambo <giambo@phobos>
2012-04-02 18:25:47 +0200

    Un paio di fixes

--- FdTViewer/src/patch_mysql_sondaggi.sql
+++ FdTViewer/src/patch_mysql_sondaggi.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS `poll` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(256) NOT NULL,
-  `text` varchar(1024) NOT NULL,
+  `text` mediumtext NOT NULL,
   `author` varchar(256) NOT NULL,
   `creationDate` datetime NOT NULL,
   `updateDate` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `poll_question` (
   `pollId` int(11) NOT NULL,
