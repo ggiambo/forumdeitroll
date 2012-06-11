b78617ed29ac18f4a89ffc53323155cd09564697
achille <achille@fdt>
2012-06-12 00:37:06 +0200

    aggiunte emoext $ghost e $anonimato

--- FdTViewer/src/com/acmetoy/ravanator/fdt/servlets/Messages.java
+++ FdTViewer/src/com/acmetoy/ravanator/fdt/servlets/Messages.java
@@ -96,6 +96,8 @@ public class Messages extends MainServlet {
 		EMO_EXT_MAP.put("angioletto2", new String[] {" $angioletto", "Angioletto 2"});
 		EMO_EXT_MAP.put("proott", new String[] {" $proott", "Proott !!"});
 		EMO_EXT_MAP.put("cool2", new String[] {" $cool", "Ficoso 2"});
+		EMO_EXT_MAP.put("anonimato", new String[] {" $anonimato", "Anonimo Animato"});
+		EMO_EXT_MAP.put("ghost", new String[] {" $ghost", "Ghost Animato"});
 	}
 
 	public static final int MAX_MESSAGE_LENGTH = 40000;
