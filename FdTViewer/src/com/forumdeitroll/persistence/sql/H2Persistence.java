4bb935b0ff5083e8db582f569b98dd777180e6ff
giambo <giambo@phobos>
2012-09-02 17:25:15 +0200

    Aggiornato metodo ricerca per H2

--- FdTViewer/src/com/forumdeitroll/persistence/sql/H2Persistence.java
+++ FdTViewer/src/com/forumdeitroll/persistence/sql/H2Persistence.java
@@ -19,32 +19,33 @@ public class H2Persistence extends GenericSQLPersistence {
 	private static final long serialVersionUID = 1L;
 
 	private static final Logger LOG = Logger.getLogger(H2Persistence.class);
-
+	
 	public void init(Properties databaseConfig) throws Exception {
 		Class.forName("org.h2.Driver");
 		String path = databaseConfig.getProperty("path");
 		String username = databaseConfig.getProperty("username");
 		String password = databaseConfig.getProperty("password");
 		String dbname = databaseConfig.getProperty("dbname");
-		String url = "jdbc:h2:file:" + path + "/" + dbname;
+		String url = "jdbc:h2:file:" + path + "/" + dbname + ";AUTO_SERVER=TRUE";
 		super.setupDataSource(url, username, password);
 	}
 
 	@Override
 	public List<MessageDTO> searchMessagesEx(String search, SearchMessagesSort sort, int limit, int page) {
-		// ATTENZIONE: Questo metodo non e` stato aggiornato
 		Connection conn = getConnection();
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		List<MessageDTO> result = new ArrayList<MessageDTO>();
-		StringBuilder query = new StringBuilder("SELECT MSG.*, FT.score AS relevance FROM FT_SEARCH_DATA(?, ?, ?) FT, MESSAGES MSG ");
+		StringBuilder query = new StringBuilder("SELECT *, COUNT(id) AS count FROM (");
+		query.append("SELECT MSG.*, FT.score AS relevance FROM FT_SEARCH_DATA(?, ?, ?) FT, MESSAGES MSG ");
 		query.append("WHERE FT.table = 'MESSAGES' AND MSG.id = FT.KEYS[0] ORDER BY ").append(sort.orderBy());
+		query.append(") GROUP by id");
 		try {
 			ps = conn.prepareStatement(query.toString());
 			ps.setString(1, search);
 			ps.setInt(2, limit);
 			ps.setInt(3, limit*page);
-			return getMessages(ps.executeQuery(), false);
+			return getMessages(ps.executeQuery(), true);
 		} catch (SQLException e) {
 			LOG.error("Cannot get messages", e);
 		} finally {
