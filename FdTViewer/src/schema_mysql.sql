2f21dc3ac52d066e37ae00b4e252151a6c53339b
achille <achille@fdt>
2013-10-09 19:50:22 +0200

    aggiornato schema per tags

--- FdTViewer/src/schema_mysql.sql
+++ FdTViewer/src/schema_mysql.sql
@@ -236,3 +236,16 @@ CREATE TABLE `tags` (
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2012-11-24 22:46:59
+
+CREATE TABLE IF NOT EXISTS `tagnames` (
+	`t_id` int(11) NOT NULL AUTO_INCREMENT,
+	`value` tinytext NOT NULL,
+	PRIMARY KEY (`t_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `tags_bind` (
+	`t_id` int(11) NOT NULL,
+	`m_id` int(11) NOT NULL,
+	`author` tinytext NOT NULL,
+	PRIMARY KEY (`t_id`, `m_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
