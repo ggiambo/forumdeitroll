3a2fc399a43d833f0e376b3a3fd46f7f2f6ab217
Giambo <giambo@lavoro>
2014-04-25 12:09:16 +0200

    Errori copy&pasta

--- FdTViewer/src/patch_fake_ads.sql
+++ FdTViewer/src/patch_fake_ads.sql
@@ -8,7 +8,7 @@ create table ads (
 insert into ads (id, title, visurl, content) values (1, 'Saune Gay‎', 'www.luino.it/stazione/', 'Trova la sauna gay ideale vicino a casa tua !');
 insert into ads (id, title, visurl, content) values (2, 'Dildi Giganti', 'www.megadildo.it', 'Regala un sorriso al tuo bucio del cuxo !');
 insert into ads (id, title, visurl, content) values (3, 'Suore per tutti‎‎', 'www.suoregratis.it', 'La tua Suora personalizzata, a prezzi imbattibili !');
-insert into ads (id, title, visurl, content) values (4, 'M5S Blah Banf‎', 'www.m5sprot.it', '>Generatore automatico di fango da gettare sul M5S');
+insert into ads (id, title, visurl, content) values (4, 'M5S Blah Banf‎', 'www.m5sprot.it', 'Generatore automatico di fango da gettare sul M5S');
 insert into ads (id, title, visurl, content) values (5, 'Foto pazzesche !', 'www.nuncipossocredere.it', 'Foto hot scattate dalla Sonda Yutsu');
 insert into ads (id, title, visurl, content) values (6, 'MILF Svizzere di qualit&agrave;', 'www.milffornothing.it', 'MILF di qualit&agrave; svizzera, prezzo tailandese !');
 insert into ads (id, title, visurl, content) values (7, 'Amuleti anti Wakko‎‎', 'www.amuletiwakkosi.it', 'Tieni lontani i wakki da questo forum, garantiti al 100%');
