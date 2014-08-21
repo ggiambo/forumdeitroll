2638c8dfe2e52175bc8f9a0b8a8de602e3c46d36
Giambo <giambo@phobos>
2014-08-21 22:06:34 +0200

    Spostati

--- FdTViewer/gen/generate.sh
+++ FdTViewer/gen/generate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp ../WebContent/WEB-INF/lib/jooq-3.3.1.jar:../WebContent/WEB-INF/lib/jooq-codegen-3.3.1.jar:../WebContent/WEB-INF/lib/jooq-meta-3.3.1.jar:../WebContent/WEB-INF/lib/mysql-connector-java-5.1.16-bin.jar:. org.jooq.util.GenerationTool /jooq-config.xml
+java -cp ../WebContent/WEB-INF/lib/jooq-3.3.1.jar:../libs/jooq-codegen-3.3.1.jar:../libs/jooq-meta-3.3.1.jar:../WebContent/WEB-INF/lib/mysql-connector-java-5.1.16-bin.jar:. org.jooq.util.GenerationTool /jooq-config.xml
