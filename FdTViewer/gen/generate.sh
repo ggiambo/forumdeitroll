5d6e5afb07295cb99abf282ebcdba1e1dc3f38fa
achille <achille@fdt>
2015-05-24 15:56:18 +0200

    generatore jooq sotto ant

--- FdTViewer/gen/generate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -cp ../WebContent/WEB-INF/lib/jooq-3.3.1.jar:../libs/jooq-codegen-3.3.1.jar:../libs/jooq-meta-3.3.1.jar:../WebContent/WEB-INF/lib/mysql-connector-java-5.1.16-bin.jar:. org.jooq.util.GenerationTool /jooq-config.xml
