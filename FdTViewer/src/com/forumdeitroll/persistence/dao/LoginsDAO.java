8c4c3c64b7d72d7b3c49f6e79ec67cc061026aeb
achille <achille@fdt>
2015-05-24 17:11:34 +0200

    errore in compilazione jooq (newbie)

--- FdTViewer/src/com/forumdeitroll/persistence/dao/LoginsDAO.java
+++ FdTViewer/src/com/forumdeitroll/persistence/dao/LoginsDAO.java
@@ -1,6 +1,6 @@
 package com.forumdeitroll.persistence.dao;
 
-import static com.forumdeitroll.persistence.jooq.tables.Logins.LOGINS;
+import com.forumdeitroll.persistence.jooq.tables.Logins;
 
 import java.sql.Timestamp;
 
@@ -17,17 +17,17 @@ public class LoginsDAO extends BaseDAO {
 
 	public String createLogin(String nick) {
 		// cleanup login vecchie oltre il minuto
-		jooq.deleteFrom(LOGINS)
-			.where(LOGINS.TSTAMP.lt(DSL.field("date_add(current_timestamp, INTERVAL -1 MINUTE)", Timestamp.class)))
+		jooq.deleteFrom(Logins.LOGINS)
+			.where(Logins.LOGINS.TSTAMP.lt(DSL.field("date_add(current_timestamp, INTERVAL -1 MINUTE)", Timestamp.class)))
 			.execute()
 		;
 		// chiave generata per accesso
 		String key = RandomPool.getString(64);
 		// inserisco il record di login
-		jooq.insertInto(LOGINS)
-			.set(LOGINS.NICK, nick)
-			.set(LOGINS.LOGINKEY, key)
-			.set(LOGINS.TSTAMP, DSL.field("current_timestamp", Timestamp.class))
+		jooq.insertInto(Logins.LOGINS)
+			.set(Logins.LOGINS.NICK, nick)
+			.set(Logins.LOGINS.LOGINKEY, key)
+			.set(Logins.LOGINS.TSTAMP, DSL.field("current_timestamp", Timestamp.class))
 			.execute()
 		;
 		return key;
