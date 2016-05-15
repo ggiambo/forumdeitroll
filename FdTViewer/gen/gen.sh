4c4889363e88b4865b89d305b49c9fd5d91dd287
Giambo <giambo@phobos>
2016-05-15 21:05:03 +0200

    Cleanup

--- FdTViewer/gen/gen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-classpath=\
-~/.m2/repository/org/jooq/jooq/3.6.1/jooq-3.6.1.jar:\
-~/.m2/repository/org/jooq/jooq-meta/3.6.1/jooq-meta-3.6.1.jar:\
-~/.m2/repository/org/jooq/jooq-codegen/3.6.1/jooq-codegen-3.6.1.jar:\
-~/.m2/repository/mysql/mysql-connector-java/5.1.34/mysql-connector-java-5.1.34.jar
-
-java -classpath $classpath org.jooq.util.GenerationTool ./jooq-config.xml
