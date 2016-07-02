15ddd0cc1a253c171c3289f116f7ab5e6ac5a688
Giambo <giambo@phobos>
2016-07-02 23:33:31 +0200

    E' ora che tu impari l'SQL, giovane padawan ...

--- FdTViewer/sql/patch_mysql_tabella_authors.sql
+++ FdTViewer/sql/patch_mysql_tabella_authors.sql
@@ -1,3 +1,16 @@
 ALTER TABLE `authors`
 ADD `creationDate` DATE NULL,
-ADD `enabled` BOOLEAN NOT NULL DEFAULT TRUE ;
+ADD `enabled` BOOLEAN NOT NULL DEFAULT TRUE;
+
+UPDATE authors
+SET authors.creationDate =
+(
+	SELECT date
+	FROM messages
+	WHERE messages.author = authors.nick
+	ORDER BY date ASC
+	LIMIT 1
+);
+
+UPDATE authors
+SET enabled = creationDate IS NOT NULL;
